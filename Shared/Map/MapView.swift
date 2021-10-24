236b14c8ed15426726ab4978d820e87e080860c8
Michal Simik <michal.simik@gmail.com>
2021-10-24 11:49:43 +0200

    feat: request and track location change

--- Shared/Map/MapView.swift
+++ Shared/Map/MapView.swift
@@ -10,26 +10,80 @@ import Combine
 import MapKit
 
 struct MapView: View {
-    @StateObject private var viewModel = MapViewModel()
+    @StateObject var viewModel = MapViewModel()
 
     var body: some View {
         ZStack {
             Map(coordinateRegion: $viewModel.region, annotationItems: viewModel.beacons ?? [], annotationContent: {
-                beacon in
                 return MapMarker(
-                    coordinate: beacon.coordinate,
+                    coordinate: $0.coordinate,
                     tint: .red
                 )
             }).onAppear {
-                viewModel.getCurrentLocation()
                 viewModel.getBeacons()
             }
-
-        Button(action: {
-            viewModel.requestLocationAuthorization()
-        }) {
-            Text("Request authorization")
-        }.background(.white).hidden()
+            LocationPermissionView().environmentObject(viewModel)
         }
+    }
+}
+
+struct LocationPermissionView: View {
+    @EnvironmentObject var viewModel: MapViewModel
+
+    var body: some View {
+        switch viewModel.authorizationStatus {
+        case .notDetermined:
+            AnyView(RequestLocationView())
+                .environmentObject(viewModel)
+        case .restricted:
+            ErrorView(errorText: "Location use is restricted.")
+        case .denied:
+            ErrorView(errorText: "The app does not have location permissions. Please enable them in settings.")
+        case .authorizedAlways, .authorizedWhenInUse:
+            EmptyView()
+        default:
+            Text("Unexpected status")
+        }
+    }
+}
+
+struct RequestLocationView: View {
+    @EnvironmentObject var viewModel: MapViewModel
+
+    var body: some View {
+        VStack {
+            Image(systemName: "location.circle")
+                .resizable()
+                .frame(width: 100, height: 100, alignment: .center)
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            Button(action: {
+                print("allowing perms")
+            }, label: {
+                Label("Allow tracking", systemImage: "location")
+            })
+            .padding(10)
+            .foregroundColor(.white)
+            .background(Color.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+            Text("We need your permission to track you.")
+                .foregroundColor(.gray)
+                .font(.caption)
+        }
+    }
+}
+
+struct ErrorView: View {
+    var errorText: String
+
+    var body: some View {
+        VStack {
+            Image(systemName: "xmark.octagon")
+                    .resizable()
+                .frame(width: 100, height: 100, alignment: .center)
+            Text(errorText)
+        }
+        .padding()
+        .foregroundColor(.white)
+        .background(Color.red)
     }
 }
